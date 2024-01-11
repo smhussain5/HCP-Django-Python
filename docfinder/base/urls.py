64196bf2f1dcfe7f53e34944a0e03e36e8af77ae
smhussain5 <139831097+smhussain5@users.noreply.github.com>
2024-01-10 22:39:28 -0600

    Added login/logout functionality

--- docfinder/base/urls.py
+++ docfinder/base/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_user, name="login"),
+    path("logout", views.logout_user, name="logout"),
     path("register", views.register_user, name="register"),
     path("physician/<int:pk>", views.by_physician, name="physician"),
     path("specialty/<int:pk>", views.by_specialty, name="specialty"),
--- docfinder/base/views.py
+++ docfinder/base/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -6,6 +7,7 @@ from .models import Physician, Specialty
 # Create your views here.
 
 
+@login_required(login_url="login")
 def index(request):
     specialties = Specialty.objects.all().order_by("name")
     physicians = Physician.objects.all().order_by("last_name")
@@ -25,12 +27,21 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, "Woo! You're logged in! 🎉")
             return redirect("/", context)
         else:
-            messages.error(request, "Uh-oh! There was an error! Try again?")
+            messages.error(request, "Uh-oh! There was an error logging you in! Try again?")
             return render(request, 'base/login.html', context)
     else:
         return render(request, 'base/login.html', context)
+
+
+def logout_user(request):
+    specialties = Specialty.objects.all().order_by("name")
+    context = {}
+    context["specialties"] = specialties
+    logout(request)
+    return redirect("/", context)
 
 
 def register_user(request):
@@ -40,6 +51,7 @@ def register_user(request):
     return render(request, 'base/register.html', context)
 
 
+@login_required(login_url="login")
 def by_physician(request, pk):
     specialties = Specialty.objects.all().order_by("name")
     physician_record = Physician.objects.get(id=pk)
@@ -49,6 +61,7 @@ def by_physician(request, pk):
     return render(request, 'base/physician.html', context)
 
 
+@login_required(login_url="login")
 def by_specialty(request, pk):
     specialties = Specialty.objects.all().order_by("name")
     specialty = Specialty.objects.get(id=pk)
